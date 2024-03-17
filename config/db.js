4cd7e082150765ca6e535aca62cd374a5e3be9fb
Nithin <94748016+Nithinlinga@users.noreply.github.com>
2024-03-17 13:42:18 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const connectDB =async()=>{
     try{
         // const conn=await mongoose.connect(process.env.MONGO_URL);
          mongoose.connect(`${process.env.MONGO_URL}`, {useNewUrlParser: true, useUnifiedTopology: true});
-        console.log(`connected ro MongoDB ${conn.connection.host}`)
+        // console.log(`connected ro MongoDB ${conn.connection.host}`)
     }catch(error){
         console.log(`Error Mongodb ${error}`) ;
     }
