d18001b895df10b6c40529f14c46f9f59690f81d
Nithin <94748016+Nithinlinga@users.noreply.github.com>
2024-03-17 14:20:39 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDB =async()=>{
     try{
         // const conn=await mongoose.connect(process.env.MONGO_URL);
-         mongoose.connect(`${process.env.MONGO_URL}`, {useNewUrlParser: true, useUnifiedTopology: true});
+         mongoose.connect(`mongodb://nithinlinga2:Nithin@ac-e1ztx5j-shard-00-00.otzisrp.mongodb.net:27017,ac-e1ztx5j-shard-00-01.otzisrp.mongodb.net:27017,ac-e1ztx5j-shard-00-02.otzisrp.mongodb.net:27017/?replicaSet=atlas-xywy94-shard-0&ssl=true&authSource=admin`, {useNewUrlParser: true, useUnifiedTopology: true});
         console.log(`connected to MongoDB ${conn.connection.host}`)
     }catch(error){
         console.log(`Error Mongodb ${error}`) ;
